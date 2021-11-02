62b0b200d48d185f8526286888dee394d20beac0
iKairos <warframeuser@yahoo.com>
2021-11-03 07:46:40 +0800

    cleaned components handling

--- _statisfy/front_end/src/screens/DashboardScreen.js
+++ _statisfy/front_end/src/screens/DashboardScreen.js
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 import { Col, Row, Button, Card} from "react-bootstrap";
 import StatScreen from "../DashboardContents/StatsMethods";
 import MLScreen from "../DashboardContents/MachineLearning";
@@ -8,43 +6,27 @@ import img1 from "../images/statisticsHeader.png";
 import img2 from "../images/mlHeader.png";
 import "../StyleSheets/dashboard.css";
 
-
+import MethodCard from "../components/MethodCard";
 
 export default function DashboardScreen(){
     return(
         <div className="wrapper">
-                    <MethodCard
-                        img = {img1}
-                        title ="Statistical Analysis"
-                        content = "Analyze and interpret the dataset you have and unlock the intricacies of relationships, associations, and patterns!"
-                        link="/dashboard/stats"
-                    />
-                    <MethodCard
-                        img = {img2}
-                        title ="Machine Learning"
-                        content = "Find patterns and generate machine learning models for your predictive and classification researches! Leave the training and model generation to us!"
-                        link ="/dashboard/machinelearning"
-                     />
+            <MethodCard
+                img = {img1}
+                title ="Statistical Analysis"
+                content = "Analyze and interpret the dataset you have using statistical methods. Leave the computation to us!"
+                link="/dashboard/stats"
+            />
+            <MethodCard
+                img = {img2}
+                title ="Machine Learning"
+                content = "Find patterns and generate machine learning models for your predictive and classification researches! Leave the training and model generation to us!"
+                link ="/dashboard/machinelearning"
+            />
         </div>
         
         
     );
-}
-
-function MethodCard(props){
-    return(
-    <div className="card">
-        <div className="card_body">
-            <img className="card_image" src ={props.img}/>
-            <h2 className= "card_title">{props.title}</h2>
-            <p className= "card_desc">
-            {props.content}
-            </p>
-        </div>
-        
-        <Link className="card_link" to={props.link}><button className ="card_btn">Proceed</button></Link>
-    </div>   
-    ); 
 }
 /*
 <Row>
