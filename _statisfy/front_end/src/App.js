8aa21bdebe2daae1fe5b684264f1a947e27aeec9
KevinBryanAban <kevaban@yahoo.com>
2021-11-02 14:28:26 +0800

    CSS
    Min

--- _statisfy/front_end/src/App.js
+++ _statisfy/front_end/src/App.js
@@ -28,29 +28,30 @@ function App() {
               <Row className ="FirstRow">
                 <Col sm ="5" md lg ="2">
                   <div>
-                      <a href>Statisfy</a>
+                     <Link class="headerLink" to="/">Statisfy</Link>
                   </div>
                 </Col>
                 <Col sm ="2" md lg ="8"/>
                 <Col sm ="5" md lg ="2">
                   <div>
-                      <a style={{alignContent:"end"}}>Account</a>
+                      <a style={{alignContent:"end", padding:"5px"}}>Log-in</a>
+                      <a style={{alignContent:"end", padding:"5px"}}>Sign up</a>
                   </div>
                 </Col>
               </Row>
               <Row className ="SecondRow">
                 <Col sm md lg ="12">
-                <Navbar bg ="dark" variant="dark" fluid>
+                <Navbar bg="light" variant="light" fluid>
                       <Nav className ="justify-content-center" style={{width:"100%"}}>
                         <Nav.Link className="NavbarContents">
-                          <Link to="/">Home</Link>
+                          <Link class="menuLink" to="/">Home</Link>
                         </Nav.Link>
                         <NavDropdown title ="Guides" className="NavbarContents">
                             <NavDropdown.Item>Statistical Method Guide</NavDropdown.Item>
                             <NavDropdown.Item>Machine Learning Guide</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link className="NavbarContents">
-                          <Link to="/dashboard">Dashboard</Link>
+                          <Link class="menuLink" to="/dashboard">Dashboard</Link>
                         </Nav.Link>
                         <Nav.Link className="NavbarContents">About</Nav.Link>
                       </Nav>
