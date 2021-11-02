6d65180e0245e705bc633066550597aaf268f333
KevinBryanAban <kevaban@yahoo.com>
2021-11-02 19:37:32 +0800

    Dashboard
    cards

--- _statisfy/front_end/src/screens/DashboardScreen.js
+++ _statisfy/front_end/src/screens/DashboardScreen.js
@@ -1,23 +1,57 @@
 import { Link } from "react-router-dom";
-import { Card } from "react-bootstrap";
 
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Button, Card} from "react-bootstrap";
+import StatScreen from "../DashboardContents/StatsMethods";
+import MLScreen from "../DashboardContents/MachineLearning";
+
+import img1 from "../images/statisticsHeader.png";
+import img2 from "../images/mlHeader.png";
+import "../StyleSheets/dashboard.css";
 
 
 
 export default function DashboardScreen(){
     return(
-        <div>
-            <Row>
-                <Link to="/dashboard/stats">Stats</Link>
-            </Row>
-            <Row>
-                  <Link to="/dashboard/machinelearning">ML</Link>
-            </Row>
-            
-            
+
+        <div className="wrapper">
+                    <MethodCard
+                        img = {img1}
+                        title ="Statistic Methods"
+                        content =" Choose this motherfucker statistical methods???"
+                        link="/dashboard/stats"
+                    />
+                    <MethodCard
+                        img = {img2}
+                        title ="Machine Learning"
+                        content =" Choose this motherfucker statistical methods???"
+                        link ="/dashboard/machinelearning"
+                     />
         </div>
         
         
     );
 }
+
+function MethodCard(props){
+    return(
+    <div className="card">
+        <div className="card_body">
+            <img className="card_image" src ={props.img}/>
+            <h2 className= "card_title">{props.title}</h2>
+            <p className= "card_desc">
+            {props.content}
+            </p>
+        </div>
+        
+            <Link className="card_link" to={props.link}><button className ="card_btn">Proceed</button></Link>
+        
+    </div>   
+    ); 
+}
+/*
+<Row>
+<Link to="/dashboard/stats">Stats</Link>
+</Row>
+<Row>
+  <Link to="/dashboard/machinelearning">ML</Link>
+</Row>*/
