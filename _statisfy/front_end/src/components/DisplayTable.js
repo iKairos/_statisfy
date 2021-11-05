f7bcf2b952970bb435f84e0d11c0aebf7c6f4210
iKairos <warframeuser@yahoo.com>
2021-11-05 20:49:39 +0800

    table render fix

--- _statisfy/front_end/src/components/DisplayTable.js
+++ _statisfy/front_end/src/components/DisplayTable.js
@@ -1,57 +1,39 @@
-import React, {useMemo} from 'react'
-import {useTable} from 'react-table'
+import React from 'react'
 import MOCK_DATA from './MOCK_DATA.json'
 import '../StyleSheets/table.css'
 
-
-
-
 export const DisplayTable = () => {
-    const data = useMemo(() => MOCK_DATA, []);
-    const columns = useMemo(() => Object.keys(data[1]).map((key, id)=>{
-          return {
-            Header: key,
-            accessor: key
-          }
-        }, [data]))
-        
-    console.log(columns);
-  
-    const tableInstance = useTable({
-      columns,
-      data
-    });
-  
-    const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow
-    } = tableInstance;
-  
+    const data = MOCK_DATA;
+
+    const columns = Object.keys(data[1]).map((key, id)=>{
+      return key;
+    })
+
+    console.log(columns)
+
     return (
-      <table {...getTableProps()}>
+      <table>
         <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-              ))}
+            <tr>
+                {
+                  columns.map(i => {return <th>{i}</th>})
+                }
             </tr>
-          ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-                })}
-              </tr>
-            );
-          })}
+        <tbody>
+            {
+              data.map(i => {
+                return(
+                  <tr>{
+                    columns.map(col =>{
+                      return(
+                        <td>{i[col]}</td>
+                      )
+                    })  
+                  }</tr>
+                )
+              })
+            }
         </tbody>
       </table>
     );
