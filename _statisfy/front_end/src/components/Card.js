b2ab794812e327e3932b6efaa928a4a6ecbdcd1e
KevinBryanAban <kevaban@yahoo.com>
2021-11-22 19:04:02 +0800

    cards

---
+++ _statisfy/front_end/src/components/Card.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+import "../StyleSheets/card.css";
+import { useState } from "react";
+
+export default function Card(){
+  //  <button className="card_btn">Select</button>
+
+    return(
+        <div className ="tool">
+            
+                <h4 className="tool_title">PearsonR</h4>
+                <div className="tool_desc">dsamdlksamdklwmalkdmsalkmdlwamdasm</div>
+                <div className= "tool_status">selected</div>
+           
+
+        </div>
+    ); 
+}
