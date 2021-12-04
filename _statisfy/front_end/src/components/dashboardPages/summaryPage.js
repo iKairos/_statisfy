96471b877c46c0d608b4b934cd44d3d9eb701115
KevinBryanAban <kevaban@yahoo.com>
2021-12-04 11:49:58 +0800

    summary

--- _statisfy/front_end/src/components/dashboardPages/summaryPage.js
+++ _statisfy/front_end/src/components/dashboardPages/summaryPage.js
@@ -32,11 +32,10 @@ export default function SummaryPage(props){
                         {<p className = "summary_body_file_content">{typeof props.DatasetDetails?.rows !== 'undefined' ? `${props.DatasetDetails?.rows} rows`: ""}</p>}
                         <span className = "summary_body_file_content">Columns Selected</span>
                         <div className = "summary_body_columns">
+                            
                             {props.Columns.map(i => {
-                                return (
-                                <div className = "summary_body_columns_cont">
-                                    <p className = "summary_body_file_content">{i}</p>
-                                </div>)
+                                return (<p>{i} </p>
+                                )
                             })}
                         </div>
                     
