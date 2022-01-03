789a4bf8e66b85fa3e67b628232dcfb60dcb93c8
KevinBryanAban <kevaban@yahoo.com>
2022-01-03 19:01:08 +0800

    profile

--- _statisfy/front_end/src/components/UserProfile.js
+++ _statisfy/front_end/src/components/UserProfile.js
@@ -1,4 +1,8 @@
 import ResearchList from "./newDashBoard/ResearchList";
+import "../StyleSheets/profilefolder/profile.css"
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Avatar } from "@mui/material";
 
 export default function UserProfile(props){
     const stringifyDatetime = datetime => {
@@ -11,16 +15,56 @@ export default function UserProfile(props){
     }
 
     return(
-        <div style={{minHeight:"5rem", paddingTop:"5rem"}}>
-            <h1>{props.user.first_name} {props.user.last_name}</h1>
-            <p>UID: {props.user._id}</p>
-            <p>Username: {props.user.username}</p>
-            <p>Email: {props.user.email_address}</p>
-            <p>Registered since: {stringifyDatetime(props.user.created_at)}</p>
-            <p>{props.user.bio}</p>
-            {props.editable && <h1>Edit</h1>}
-
+        <div className = "profile">
+            <div className="profile_avatar_container">
+            <div className="profile_avatar">
+                <div className="profile_avatar_icon">
+                    <Avatar
+                        alt= {props.user.first_name}
+                        sx={{ width: 128, height: 128 }}
+                    />
+                </div>
+                
+                <div className="profile_name">
+                    <div className="profile_name_container">
+                        <Typography 
+                            variant ="h5" 
+                            sx={{ fontWeight: 700}} 
+                            display="block" 
+                            gutterBottom
+                            color="white"
+                        >
+                            {props.user.username}
+                        </Typography>
+                    </div>
+                    <div className="profile_name_container">
+                        <Typography 
+                            variant ="h6" 
+                            sx={{ padding: 0, margin: 0}} 
+                            display="block" gutterBottom
+                            color="white"
+                        >
+                            {props.user.first_name} {props.user.last_name} 
+                        </Typography>
+                        <Typography variant="overline" display="block" gutterBottom color="white"> FULLNAME</Typography>
+                    </div>
+                   
+                    
+                    
+                    </div>
+                </div>
+            </div>
            
+            <div className="profile_details">
+                <Typography variant="overline" display="block" gutterBottom>UID : {props.user._id}</Typography>
+                <Typography variant="overline" display="block" gutterBottom>EMAIL : {props.user.email_address}</Typography>
+                
+                <Typography variant="overline" display="block" gutterBottom>JOINED: {stringifyDatetime(props.user.created_at)} </Typography>
+                <Typography variant="overline" display="block" gutterBottom>bio:{props.user.bio} </Typography>
+            
+            </div>
+            
+            
         </div>
     ); 
 }
