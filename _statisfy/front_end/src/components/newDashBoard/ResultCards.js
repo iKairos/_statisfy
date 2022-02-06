85a6a01b9663940f711d8b6bcd9d8bacd4214ec6
KevinBryanAban <kevaban@yahoo.com>
2022-02-06 18:59:57 +0800

    cards v2

--- _statisfy/front_end/src/components/newDashBoard/ResultCards.js
+++ _statisfy/front_end/src/components/newDashBoard/ResultCards.js
@@ -10,22 +10,10 @@ import Button from '@mui/material/Button';
 import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
 import useMeasure from 'react-use-measure'
 import { variableTooltip } from '../../constants/stringConstants';
+import AssessmentIcon from '@mui/icons-material/Assessment';
 
 
 export default function ResultCards(props) {
-    const [ref, { width }] = useMeasure()
-    const [onTrigger, setTrigger] = useState(false);
-    const matches = useMediaQuery('(min-width:900px)');
-
-    const cardOpen = useSpring({
-        to: { 
-            width: onTrigger ? width :0,
-        },
-        from: { width: 0}
-    })
-    
-
-
 
     const HtmlTooltip = styled(({ className, ...props }) => (
         <Tooltip {...props} classes={{ popper: className }} />
@@ -39,50 +27,27 @@ export default function ResultCards(props) {
         },
         }));
   return (
-      <>
-    {matches 
-        ? (
             <HtmlTooltip
                 title={
-                    onTrigger
-                    ?(<React.Fragment>
+                   <React.Fragment>
                         <Typography color="inherit">{props.variable}</Typography>
                         {variableTooltip[props.variable]}
-                    </React.Fragment>)
-                    :""
+                    </React.Fragment>
                 }
                 placement="bottom-start"
             >
-            <div className="Study_cards" onClick={()=>setTrigger(!onTrigger)}  >  
-                
-                    <div className='Study_cards_text'  >
-                        <Typography sx={{ fontSize: "1.5rem", fontWeight: 600 }}> {props.value}</Typography>
-                        <Typography variant="button" sx={{color:"#a742c5"}}>{props.variable}</Typography>
-                    </div>
+                <div className="Study_cards">  
                     
-                    <animated.div style = {cardOpen} >
-                        {onTrigger
-                        ? (<IconButton ref= {ref} ><ContentCopyIcon/></IconButton>)
-                        : null
-                        }
-                     </animated.div>     
-            </div>
-                    
-        </HtmlTooltip>
-        
-        )
-        :(
-            <div className="Study_cards" onClick={()=>setTrigger(!onTrigger)}>
-                <div className='Study_cards_text'>
-                    <Typography sx={{ fontSize: "1.5rem", fontWeight: 600 }}> {props.value}</Typography>
-                    <Typography variant="button" sx={{color:"#a742c5"}}>{props.variable}</Typography>
+                        <div className='Study_cards_text'>
+                            <AssessmentIcon fontSize='medium' color="secondary"/>
+                            <Typography variant="button" sx={{color:"#a742c5"}}>{props.variable}</Typography>
+                            <p className="Study_cards_text_value"> {props.value}</p>
+                            
+                        </div>  
                 </div>
-                <IconButton><ContentCopyIcon/></IconButton>                     
-            </div>
-        )
+                    
+            </HtmlTooltip>
     
-    }
-    </>
     
   );
 }
